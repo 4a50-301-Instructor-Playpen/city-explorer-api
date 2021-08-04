d036d99543cc77132c73d0f462471441849a4c34
JP <jpjones22@gmail.com>
2021-08-03 21:13:33 -0700

    refactor movie image

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ class Movie {
     this.overview = movie.overview
     this.average_votes = movie.vote_average
     this.total_votes = movie.vote_count
-    this.image_url = movie.poster_path
+    this.image_url = `https://image.tmdb.org/t/p/w200${movie.poster_path}`
     this.popularity = movie.popularity
     this.released_on = movie.release_date
   }
