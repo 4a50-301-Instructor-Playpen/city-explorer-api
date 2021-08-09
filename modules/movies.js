c97c6d3d38597a925056c4b81ff795b980fd80bb
JP <jpjones22@gmail.com>
2021-08-09 14:25:00 -0700

    shifted image error checking to server

--- modules/movies.js
+++ modules/movies.js
@@ -4,11 +4,15 @@ const axios = require('axios');
 module.exports = getMovies;
 
 function getMovies(cityName) {
+  console.log('cityname', cityName);
   const movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`;
   //console.log('Movie API URL:', movieURL);
   let movieResp = axios.get(movieURL)
     .then(mv => {
-      return parseMovieData(mv);
+      //console.log('mv:', mv)
+      let parsed = parseMovieData(mv);
+      console.log(`mvParsed: ${parsed}`);
+      return parsed;
     })
     .catch(e => console.log('Error in requesting movies:', e));
   return movieResp;
@@ -16,7 +20,13 @@ function getMovies(cityName) {
 
 function parseMovieData(mvArr) {
   try {
-    let mvArrObj = mvArr.data.results.map(m => { return new Movie(m) });
+    console.log('mvArr:', mvArr.data.results.length);
+    let mvArrObj = mvArr.data.results.map(m => {
+      if (!m.overview) m.overview = 'No OverviewProvided';
+      if (!m.poster_path) m.poster_path = 'https://via.placeholder.com/200'
+      return new Movie(m)
+    });
+    //console.log('mvObj', mvArrObj);
     return Promise.resolve(mvArrObj);
   }
   catch (e) {
