a87f4d6fc266c1253b430121204bde9e320c3e26
JP <jpjones22@gmail.com>
2021-08-09 14:31:06 -0700

    corrected poster_path in movie API

--- modules/movies.js
+++ modules/movies.js
@@ -23,7 +23,7 @@ function parseMovieData(mvArr) {
     console.log('mvArr:', mvArr.data.results.length);
     let mvArrObj = mvArr.data.results.map(m => {
       if (!m.overview) m.overview = 'No OverviewProvided';
-      if (!m.poster_path) m.poster_path = 'https://via.placeholder.com/200'
+      (!m.poster_path) ? m.poster_path = 'https://via.placeholder.com/200' : m.poster_path = `https://image.tmdb.org/t/p/w200${m.poster_path}`
       return new Movie(m)
     });
     //console.log('mvObj', mvArrObj);
@@ -39,7 +39,7 @@ class Movie {
     this.overview = movie.overview
     this.average_votes = movie.vote_average
     this.total_votes = movie.vote_count
-    this.image_url = `https://image.tmdb.org/t/p/w200${movie.poster_path}`
+    this.image_url = movie.poster_path
     this.popularity = movie.popularity
     this.released_on = movie.release_date
   }
